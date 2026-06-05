82be7fc0c397a3beeae0917754a968fdebb3ba80
HaiCLOP <haiclop6@gmail.com>
2026-06-06 00:37:29 +0530

    Fix next config serverActions schema

--- next.config.ts
+++ next.config.ts
@@ -24,11 +24,11 @@ const nextConfig: NextConfig = {
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
         ]
-      }
-    ]
   },
-  serverActions: {
-    allowedOrigins: ["freo.haicloplabs.in", "localhost:3000"]
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["freo.haicloplabs.in"]
+    }
   }
 };
 
